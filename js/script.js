b39e7db3faf8e72265d540a2d688f15c4a256614
RodinaAdelMahmoud <112697275+RodinaAdelMahmoud@users.noreply.github.com>
2023-05-12 23:58:49 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,90 @@
+$("#signup").click(function() {
+    $("#first").fadeOut("fast", function() {
+    $("#second").fadeIn("fast");
+    });
+    });
+    
+    $("#signin").click(function() {
+    $("#second").fadeOut("fast", function() {
+    $("#first").fadeIn("fast");
+    });
+    });
+    
+    
+      
+             $(function() {
+               $("form[name='login']").validate({
+                 rules: {
+                   
+                   email: {
+                     required: true,
+                     email: true
+                   },
+                   password: {
+                     required: true,
+                     
+                   }
+                 },
+                  messages: {
+                   email: "Please enter a valid email address",
+                  
+                   password: {
+                     required: "Please enter password",
+                    
+                   }
+                   
+                 },
+                 submitHandler: function(form) {
+                   form.submit();
+                 }
+               });
+             });
+             
+
+
+
+             function validate(){
+              var email = document.getElementById("email1").value;
+              var password = document.getElementById("password1").value;
+              if ( email == "admin@gmail.com" && password == "admin"){
+                  alert("login successful");
+          window.open("dashboard.html");
+              }
+              else{
+                  alert("Invalid Email and Password");
+              }
+          }
+    
+    
+    $(function() {
+      
+      $("form[name='registration']").validate({
+        rules: {
+          firstname: "required",
+          lastname: "required",
+          email: {
+            required: true,
+            email: true
+          },
+          password: {
+            required: true,
+            minlength: 5
+          }
+        },
+        
+        messages: {
+          firstname: "Please enter your firstname",
+          lastname: "Please enter your lastname",
+          password: {
+            required: "Please provide a password",
+            minlength: "Your password must be at least 5 characters long"
+          },
+          email: "Please enter a valid email address"
+        },
+      
+        submitHandler: function(form) {
+          form.submit();
+        }
+      });
+    });
+    
